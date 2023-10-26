d2ea68b34e7d90d2ae18fa99cacc4694e32520bd
Theo <trm109@case.edu>
2023-10-26 17:49:43 -0400

    2

--- pt1.cpp
+++ pt1.cpp
@@ -22,14 +22,47 @@ int main(){
   //# Unified Shared Memory Allocation enables data access on host and device
   int *data = malloc_shared<int>(N, q);
 
-  //# Initialization
-  for(int i=0; i<N; i++) data[i] = i;
-
+  //# Initialization of input matricies.
+  std::vector<float> A = generate_matrix(N, 1);
+  std::vector<float> B = generate_matrix(N, 2);
+  //# Create output matrix.
+  std::vector<float> C(N*N);
+  //# Create buffers.
+  buffer bufA(A);
+  buffer bufB(B);
+  buffer bufC(C);
   //# Offload parallel computation to device
-
+  q.submit([&](handler &h){
+    //# Create accessors.
+    accessor aA(bufA, h, read_only);
+    accessor aB(bufB, h, read_only);
+    accessor aC(bufC, h, write_only);
+    
+    //# Define size.
+    range<2> global_size(N, N);
+    range<2> work_group_size(4, 4);
+    //# Parallel computation.
+    h.parallel_for(nd_range<2>(global_size, work_group_size), [=](nd_item<2> it){
+      //# Get global id.
+      int i = it.get_global_id(0);
+      int j = it.get_global_id(1);
+      //# Compute from local mem.
+      float sum = 0.f;
+      for (int k = 0; k < N; k++) {
+        sum += aA[i*N+k] * aB[k*N+j];
+      }
+      //# Store to global mem.
+      aC[i*N+j] = sum;
+    });
+  });
+  host_accessor hC(bufC, read_only);
   //# Print Output
-  for(int i=0; i<N; i++) std::cout << data[i] << "\n";
-
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++){
+      std::cout << hC[i*N+j] << " ";
+    }
+    std::cout << std::endl;
+  }
   free(data, q);
   std::cout << "Completed" << std::endl;
   return 0;
